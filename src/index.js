aee322dc02a47b7325e5367c6a330d90d17d5d60
ashleyruizca <156567181+ashleyruizca@users.noreply.github.com>
2024-04-06 00:39:09 -0400

    added basic form handing and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatorRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "ingredients",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generatorRecipe);
