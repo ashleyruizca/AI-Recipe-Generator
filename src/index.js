ba5f46b516e7e4dca74e5fd96f55f498b70151e3
ashleyruizca <156567181+ashleyruizca@users.noreply.github.com>
2024-04-07 21:09:28 -0400

    Added API Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatorRecipe(event) {
-  event.preventDefault();
-
+function displayIngredients(response) {
   new Typewriter("#recipe", {
-    strings: "ingredients",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatorRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "4dab170858822f369f2t3478cb64o2fe";
+  let prompt = `User instructions: Generate a recipe about ${instructionsInput.value}`;
+  let context =
+    "You are a professional cook, who can cook all around the world, your expertise is to make a quick and easy recipes; your challenge is to provide all the ingredients in basic HTML. Be nice and gentle while providing the ingredients";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayIngredients);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
